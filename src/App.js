6a4b4c579f20668d831141f4769e451b0bda0221
Jesus <chuygs12@gmail.com>
2023-01-05 16:15:58 -0600

    add input to filter the countries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState("")
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(res => (setCountries(res.data)))
+  }, [])
+  
+  const filteredArray = countries.filter((country => {
+    return country.name.common.toLowerCase().slice(0, filter.length) === filter.toLowerCase()
+  }))
+
+  const arrayToRender = filteredArray.map(country => 
+  <p> {country.name.common} </p>) 
+
+  const handleChange = (event) => {
+    setFilter(event.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Data for countries</h2>
+      Find countries <input onChange={handleChange} value={filter}/>
+      <div>
+        {filter ? arrayToRender : "No coincidences-"}
+      </div>
     </div>
   );
 }
